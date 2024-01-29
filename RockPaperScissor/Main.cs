0424323bd3d125a809b10f20d63623497583c788
Antonio Durao <133801876+Antoniodurao@users.noreply.github.com>
2024-01-29 14:57:39 +0000

    Uploaded Files for the project

---
+++ RockPaperScissor/Main.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static RockPaperScissor.Enum;
+
+namespace RockPaperScissor
+{
+    internal class rps
+    {
+        static void Main(string[] args)
+        {
+            Rules rules = new Rules();
+            Enum p1 = new Enum();
+            Enum p2 = new Enum();    
+            while (true)
+            {
+                Console.WriteLine("Player 1 chose your hand: Rock || Paper || Scissor ");
+                p1.Name = Console.ReadLine();
+                while (p1.Name != "Scissor" && p1.Name != "Rock" && p1.Name != "Paper")
+                {
+                    Console.WriteLine("Please chose a normal option ");
+                    p1.Name = Console.ReadLine();
+                }
+
+                Console.WriteLine("Player 2 chose your hand: Rock || Paper || Scissor ");
+                p2.Name = Console.ReadLine();
+                while (p2.Name != "Scissor" && p2.Name != "Rock" && p2.Name != "Paper")
+                {
+                    Console.WriteLine("Please chose a normal option  ");
+                    p2.Name = Console.ReadLine();
+                }
+                    
+
+                rules.setHand(p1, p2);
+            
+                Console.WriteLine(rules.getResult());
+                Console.WriteLine("-----------------------------------------------------");
+            }
+            
+            
+
+        }
+    }
+}
---
+++ RockPaperScissor/Rules.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor
+{
+    class Rules
+    {
+        private string h1;
+        private string h2;
+        public void setHand (Enum h1, Enum h2)
+        {
+            this.h1 = h1.Name;   
+            this.h2 = h2.Name;
+        }
+
+        public string getResult ()
+        {
+            Console.WriteLine("Player 1's hand is " + this.h1);
+            Console.WriteLine("Player 2's hand is " + this.h2);
+
+            if(h1 == null || h2 == null)
+            {
+                return "No Hand has been picked..";
+            }
+
+            if((h1 == "Paper" && h2 == "Rock") || (h1 == "Rock" && h2 == "Scissor") || (h1 == "Scissor" && h2 == "Paper"))
+            {
+                return "Player 1 Wins!";
+            } else if ((h1 == h2))
+            {
+                return "It's a Draw";
+            } else
+            {
+                return "Player 2 Wins!";
+            }
+        }
+    }
+}
